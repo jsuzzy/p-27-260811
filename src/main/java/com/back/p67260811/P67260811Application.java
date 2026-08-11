47ee2b293c94ccd083b18465f3fe6b3608c08d50
jeongsuji <sujijeong11@gmail.com>
2026-08-11 09:19:52 +0900

    init

---
+++ src/main/java/com/back/p67260811/P67260811Application.java
@@ -0,0 +1,13 @@
+package com.back.p67260811;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P67260811Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(P67260811Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/back/p67260811/P67260811ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.back.p67260811;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P67260811ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
