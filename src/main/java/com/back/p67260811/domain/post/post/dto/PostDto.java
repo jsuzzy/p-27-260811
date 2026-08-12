b57d48403e1d31887ef586e3ed9d7ac604afb02f
jeongsuji <sujijeong11@gmail.com>
2026-08-12 10:06:46 +0900

    PostDto-record

--- src/main/java/com/back/p67260811/domain/post/post/dto/PostDto.java
+++ src/main/java/com/back/p67260811/domain/post/post/dto/PostDto.java
@@ -1,23 +1,24 @@
 package com.back.p67260811.domain.post.post.dto;
 
 import com.back.p67260811.domain.post.post.entity.Post;
-import lombok.Getter;
 
 import java.time.LocalDateTime;
 
-@Getter
-public class PostDto {
-    private int id;
-    private String title;
-    private String body;
-    private LocalDateTime createDate;
-    private LocalDateTime modifyDate;
+public record PostDto(
+        int id,
+        LocalDateTime createDate,
+        LocalDateTime modifyDate,
+        String subject,
+        String body
+) {
 
     public PostDto(Post post){
-        this.id = post.getId();
-        this.title = post.getTitle();
-        this.body = post.getContent();
-        this.createDate = post.getCreateDate();
-        this.modifyDate = post.getModifyDate();
+        this(
+                post.getId(),
+                post.getCreateDate(),
+                post.getModifyDate(),
+                post.getTitle(),
+                post.getContent()
+        );
     }
 }
