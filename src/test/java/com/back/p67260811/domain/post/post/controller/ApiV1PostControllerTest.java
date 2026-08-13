6b1a6dcfca671156280af7f553841621bdb451a6
jeongsuji <sujijeong11@gmail.com>
2026-08-13 12:36:45 +0900

    post- list, write test

--- src/test/java/com/back/p67260811/domain/post/post/controller/ApiV1PostControllerTest.java
+++ src/test/java/com/back/p67260811/domain/post/post/controller/ApiV1PostControllerTest.java
@@ -1,12 +1,20 @@
 package com.back.p67260811.domain.post.post.controller;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.transaction.annotation.Transactional;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest //빈이 다 등록되면 테스트를 진행해라
 @ActiveProfiles("test")
@@ -18,8 +26,40 @@ public class ApiV1PostControllerTest {
     private MockMvc mvc;
 
     @Test
-    void t1() {
+    @DisplayName("글 다건 조회")
+    void t1() throws Exception {
 
+        ResultActions resultActions = mvc
+                .perform(
+                        get("/api/v1/posts")
+                )
+                .andDo(print()); //응답을 콘솔에 출력
+
+        resultActions
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("글 생성")
+    void t2() throws Exception {
+        String title = "제목입니다";
+        String content = "내용입니다";
+
+        ResultActions resultActions = mvc
+                .perform(
+                        post("/api/v1/posts")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content("""
+                                        {
+                                            "title": "%s",
+                                            "content": "%s"
+                                        }
+                                        """.formatted(title, content))
+                )
+                .andDo(print());
+
+        resultActions
+                .andExpect(status().isCreated());
     }
 
 }
