1d2c5da6a500e2184ef20d0f10aa987c1ccd7257
jeongsuji <sujijeong11@gmail.com>
2026-08-14 11:36:35 +0900

    GlobalExceptionHandler-MethodArgumentNotValidException

--- src/main/java/com/back/p67260811/global/exceptionHandler/GlobalExceptionHandler.java
+++ src/main/java/com/back/p67260811/global/exceptionHandler/GlobalExceptionHandler.java
@@ -1,21 +1,44 @@
 package com.back.p67260811.global.exceptionHandler;
 
 import com.back.p67260811.global.dto.RsData;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.Comparator;
 import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
 
 @ControllerAdvice //Controller 안에서 발생하는 예외 한에서 예외를 잡음
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(NoSuchElementException.class) //매개변수로 전달된 예외의 처리를 아래 메서드가 처리
     @ResponseBody
-    public RsData<Void> handleException(){
+    public RsData<Void> noSuchElementException(){
         return new RsData<Void>(
                 "404-1",
                 "존재하지 않는 데이터입니다."
+        );
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class) //매개변수로 전달된 예외의 처리를 아래 메서드가 처리
+    @ResponseBody
+    public RsData<Void> methodArgumentNotValidException(MethodArgumentNotValidException e){
+
+        String message = e.getBindingResult()
+                .getAllErrors()
+                .stream()
+                .filter(error -> error instanceof FieldError)
+                .map(error -> (FieldError) error)
+                .map(error -> error.getField() + "-" + error.getCode() + "-" + error.getDefaultMessage())
+                .sorted(Comparator.comparing(String::toString))
+                .collect(Collectors.joining("\n"));
+
+        return new RsData<Void>(
+                "400-1",
+                message
         );
     }
 
