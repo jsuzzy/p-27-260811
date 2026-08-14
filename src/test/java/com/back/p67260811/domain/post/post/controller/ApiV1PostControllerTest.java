c4e2e40bb201a5c950c190895f974cf21336de68
jeongsuji <sujijeong11@gmail.com>
2026-08-14 11:52:34 +0900

    MethodArgumentNotValidException test

--- src/test/java/com/back/p67260811/domain/post/post/controller/ApiV1PostControllerTest.java
+++ src/test/java/com/back/p67260811/domain/post/post/controller/ApiV1PostControllerTest.java
@@ -180,5 +180,63 @@ public class ApiV1PostControllerTest {
         assertThat(post).isNull();
     }
 
+    @Test
+    @DisplayName("글 작성, 제목이 입력되지 않은 경우")
+    void t6() throws Exception {
+        String title = "";
+        String content = "내용입니다";
+
+        ResultActions resultActions = mvc
+                .perform(
+                        post("/api/v1/posts")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content("""
+                                        {
+                                            "title": "%s",
+                                            "content": "%s"
+                                        }
+                                        """.formatted(title, content))
+                )
+                .andDo(print());
+
+        resultActions
+                .andExpect(handler().handlerType(ApiV1PostController.class))
+                .andExpect(handler().methodName("write"))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.resultCode").value("400-1"))
+                .andExpect(jsonPath("$.msg").value(" title-NotBlank-제목을 입력해 주세요.\ntitle-Size-제목은 2글자 이상 10글자 이하로 작성해 주세요. "
+                        .stripIndent().trim()));
+
+
+    }
+
+    @Test
+    @DisplayName("글 작성, 내용이 입력되지 않은 경우")
+    void t7() throws Exception {
+        String title = "제목입니다.";
+        String content = "";
+
+        ResultActions resultActions = mvc
+                .perform(
+                        post("/api/v1/posts")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content("""
+                                        {
+                                            "title": "%s",
+                                            "content": "%s"
+                                        }
+                                        """.formatted(title, content))
+                )
+                .andDo(print());
+
+        resultActions
+                .andExpect(handler().handlerType(ApiV1PostController.class))
+                .andExpect(handler().methodName("write"))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.resultCode").value("400-1"))
+                .andExpect(jsonPath("$.msg").value("content-NotBlank-내용을 입력해 주세요.\ncontent-Size-내용은 2글자 이상 10글자 이하로 작성해 주세요."
+                        .stripIndent().trim()));
+    }
+
 
 }
